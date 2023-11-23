56f70f447ad61e83e5b0f79ae64c317babb91f81
Muhammed Shuraif <shuraifpilathara@gmail.com>
2023-11-23 21:12:03 +0530

    History page added

--- src/App.js
+++ src/App.js
@@ -8,9 +8,12 @@ import { BrowserRouter as Router, Route, Routes, Link, Navigate } from 'react-ro
 
 import RegistrationForm from './screens/SignUp';
 import SignIn from './screens/SignIn';
-import SendMail from './screens/SendMail';
+import SendMail from './screens/sendmail/SendMail';
+import SendMailHome from './screens/sendmail/SendMailHome';
 import Settings from './screens/Settings';
 import Profile from './screens/Profile';
+import History from './screens/history/History';
+
 
 import { useSelector, useDispatch } from 'react-redux';
 import { Home } from '@mui/icons-material';
@@ -19,35 +22,64 @@ import {
   isLoginStatus,
 } from './redux/counterSlice';
 
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 const App = () => {
 
   const isLogin = useSelector(isLoginStatus);
-
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: '#123E91', // Custom primary color
+      },
+      secondary: {
+        main: '#ff4081', // Custom secondary color
+      },
+      background: {
+        default: '#555555',
+        paper: '#f5f5f5',
+      }
+    },
+    typography: {
+      fontFamily: 'Roboto, sans-serif',
+      fontSize: 16,
+      fontWeightRegular: 400,
+      fontWeightBold: 700,
+    },
+    globals: {
+      '@global': {
+        body: {
+          backgroundColor: '#f5f5f5',
+        },
+      },
+    }
+    // Add more customizations as needed
+  });
 
   return (
-    <Router>
+    <ThemeProvider theme={theme}>
+      <Router>
 
-      {
-        !isLogin ? <>
-          <AppDrawer />
-          <Routes>
-            <Route index element={<SendMail />} />
-            <Route path="/sendmail" element={<SendMail />} />
-            <Route path="/settings" element={<Settings />} />
-            <Route path="/profile" element={<Profile />} />
-          </Routes>
-        </> :
-          <>
+        {
+          isLogin ? <>
+            <AppDrawer />
             <Routes>
-              <Route index element={<SignIn />} />
-              <Route path='/signup' element={<RegistrationForm />} />
-              <Route path='/login' element={<SignIn />} />
+              <Route index element={<SendMailHome />} />
+              <Route path="/sendmail" element={<SendMailHome />} />
+              <Route path="/settings" element={<Settings />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/history" element={<History/>} />
             </Routes>
-          </>
-      }
-
-
-    </Router>
+          </> :
+            <>
+              <Routes>
+                <Route index element={<SignIn />} />
+                <Route path='/signup' element={<RegistrationForm />} />
+                <Route path='/login' element={<SignIn />} />
+              </Routes>
+            </>
+        }
+      </Router>
+    </ThemeProvider>
   )
 }
 
