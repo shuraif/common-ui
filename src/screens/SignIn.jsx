7f8d483e1d1b7db3456ebed364ae48d93cea49d8
shuraif <shuraifpilathara@gmail.com>
2023-11-24 18:40:13 +0530

    transitions added

--- src/screens/SignIn.jsx
+++ src/screens/SignIn.jsx
@@ -12,14 +12,14 @@ import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { Link ,useNavigate} from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import { useSelector, useDispatch } from 'react-redux';
 import {
   login,
   isLoginStatus,
 } from '../redux/counterSlice';
-
+import Grow from '@mui/material/Grow';
 
 function Copyright(props) {
   return (
@@ -43,107 +43,144 @@ export default function SignIn() {
   const isLogin = useSelector(isLoginStatus);
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const [passsword, setPassword] = React.useState('')
+  const [checked, setChecked] = React.useState(false);
+  const [userName, setUserName] = React.useState('');
+  const [errorMessage, setErrorMessage] = React.useState('')
 
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    const data = new FormData(event.currentTarget);
+    const data = []
     const request = {
-      email: data.get('email'),
-      password: data.get('password')
+      email: userName,
+      password: passsword
     }
 
-    axios.post('http://localhost:8080/api/general/logincust',request)
-    .then( (res)=> {
-      console.log(res.data)
-       if(res.data.customerGuid!=null){
-        console.log('login success')
-        navigate('/');
-       }else{
-        console.log('login failed')
-       }
-        
-         
-    })
-    .catch(function (error) {
+    axios.post('http://localhost:8090/api/general/logincust', request)
+      .then((res) => {
+        console.log(res.data)
+        if (res.data.customerGuid != null) {
+          console.log('login success')
+          dispatch(login())
+          //navigate('/');
+        } else {
+          console.log('login failed')
+          setErrorMessage('Login failed')
+        }
+
+
+      })
+      .catch(function (error) {
         // handle error
         console.log(error);
-    })
-    .finally(function () {
+      })
+      .finally(function () {
         // always executed
-    });
+      });
+  };
+
+
+
+  const handleUserNameChange = (event) => {
+    const newValue = event.target.value;
+    setErrorMessage('')
+    setUserName(newValue);
+    console.log(newValue);
+  };
+
+  const handleUserPasswordChange = (event) => {
+    const newValue = event.target.value;
+    setErrorMessage('')
+    setPassword(newValue);
+    console.log(newValue);
   };
 
   return (
     <ThemeProvider theme={defaultTheme}>
       <Container component="main" maxWidth="xs">
         <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
+        <Grow
+          in={true}
+          style={{ transformOrigin: '0 100 0' }}
+          {...(checked ? { timeout: 1000 } : {})}
         >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign in
-          </Typography>
-          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-            <TextField
-              margin="normal"
-              required
+          <Box
+            sx={{
+              marginTop: 8,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Sign in
+            </Typography>
+            <Box component="form" noValidate sx={{ mt: 1 }}>
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                value={userName}
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+                onChange={handleUserNameChange}
+              />
+              <TextField
+                value={passsword}
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                onChange={handleUserPasswordChange}
+              />
+              {/* <FormControlLabel
               fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-            />
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
-             <span >{isLogin?"Login done ":"login not done"}</span>
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
+                control={<Checkbox value="remember" color="primary" />}
+                label="Remember me"
+              /> */}
+              <br />
+              <div style={{ color: 'red', display: 'flex', justifyContent: 'center' }}>
+                {errorMessage}
+              </div>
 
-              onClick={() => 
-                dispatch(login())
-              }
-              
-            >
-              Sign In
-            </Button>
-            <Grid container>
-              <Grid item xs>
-                <Link href="#" variant="body2">
-                  Forgot password?
-                </Link>
+
+
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+
+                onClick={handleSubmit}
+
+              >
+                Sign In
+              </Button>
+              <Grid container>
+                <Grid item xs>
+                  <Link href="#" variant="body2">
+                    Forgot password?
+                  </Link>
+                </Grid>
+                <Grid item>
+                  <Link to="/signup"> Don't have an account? Sign Up</Link>
+
+                </Grid>
               </Grid>
-              <Grid item>
-              <Link to="/signup"> Don't have an account? Sign Up</Link>
-               
-              </Grid>
-            </Grid>
+            </Box>
           </Box>
-        </Box>
+        </Grow>
         <Copyright sx={{ mt: 8, mb: 4 }} />
       </Container>
     </ThemeProvider>
--- src/screens/SignUp.jsx
+++ src/screens/SignUp.jsx
@@ -13,11 +13,12 @@ import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 import axios from 'axios';
-import { Link ,useNavigate} from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import {
-  login,
+    login,
 } from '../redux/counterSlice';
+import Grow from '@mui/material/Grow';
 
 const defaultTheme = createTheme();
 
@@ -40,12 +41,12 @@ export default function SignUp() {
         }
         console.log(request);
 
-        axios.post('http://localhost:8080/api/general/registercust',request)
-            .then( (res)=> {
+        axios.post('http://localhost:8090/api/general/registercust', request)
+            .then((res) => {
                 // handle success
                 //console.log(JSON.stringify(res))
                 console.log(res.data)
-                navigate('/'); 
+                navigate('/');
             })
             .catch(function (error) {
                 // handle error
@@ -56,114 +57,125 @@ export default function SignUp() {
             });
     };
 
+    const [checked, setChecked] = React.useState(false);
+
+    const handleChange = () => {
+        setChecked((prev) => !prev);
+    };
+
     return (
         <ThemeProvider theme={defaultTheme}>
             <Container component="main" maxWidth="xs">
                 <CssBaseline />
-                <Box
-                    sx={{
-                        marginTop: 8,
-                        display: 'flex',
-                        flexDirection: 'column',
-                        alignItems: 'center',
-                    }}
+                <Grow
+                    in={true}
+                    style={{ transformOrigin: '0 100 0' }}
+                    {...(checked ? { timeout: 1000 } : {})}
                 >
-                    <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-                        <LockOutlinedIcon />
-                    </Avatar>
-                    <Typography component="h1" variant="h5">
-                        Sign up
-                    </Typography>
-                    <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
-                        <Grid container spacing={2}>
-                            <Grid item xs={12} sm={6}>
-                                <TextField
-                                    autoComplete="given-name"
-                                    name="firstName"
-                                    required
-                                    fullWidth
-                                    id="firstName"
-                                    label="First Name"
-                                    autoFocus
-                                />
+                    <Box
+                        sx={{
+                            marginTop: 8,
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignItems: 'center',
+                        }}
+                    >
+                        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                            <LockOutlinedIcon />
+                        </Avatar>
+                        <Typography component="h1" variant="h5">
+                            Sign up
+                        </Typography>
+                        <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+                            <Grid container spacing={2}>
+                                <Grid item xs={12} sm={6}>
+                                    <TextField
+                                        autoComplete="given-name"
+                                        name="firstName"
+                                        required
+                                        fullWidth
+                                        id="firstName"
+                                        label="First Name"
+                                        autoFocus
+                                    />
+                                </Grid>
+                                <Grid item xs={12} sm={6}>
+                                    <TextField
+                                        required
+                                        fullWidth
+                                        id="lastName"
+                                        label="Last Name"
+                                        name="lastName"
+                                        autoComplete="family-name"
+                                    />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <TextField
+                                        required
+                                        fullWidth
+                                        id="phone"
+                                        label="Phone Number"
+                                        name="phone"
+                                        autoComplete="phone"
+                                    />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <TextField
+                                        required
+                                        fullWidth
+                                        id="email"
+                                        label="Email Address"
+                                        name="email"
+                                        autoComplete="email"
+                                    />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <TextField
+                                        required
+                                        fullWidth
+                                        name="password"
+                                        label="Password"
+                                        type="password"
+                                        id="password"
+                                        autoComplete="new-password"
+                                    />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <TextField
+                                        required
+                                        fullWidth
+                                        name="confirm password"
+                                        label="Confirm Password"
+                                        type="password"
+                                        id="confirmPassword"
+                                        autoComplete="confirm-password"
+                                    />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <FormControlLabel
+                                        control={<Checkbox value="allowExtraEmails" color="primary" />}
+                                        label="Accept privacy policy"
+                                    />
+                                </Grid>
                             </Grid>
-                            <Grid item xs={12} sm={6}>
-                                <TextField
-                                    required
-                                    fullWidth
-                                    id="lastName"
-                                    label="Last Name"
-                                    name="lastName"
-                                    autoComplete="family-name"
-                                />
-                            </Grid>
-                            <Grid item xs={12}>
-                                <TextField
-                                    required
-                                    fullWidth
-                                    id="phone"
-                                    label="Phone Number"
-                                    name="phone"
-                                    autoComplete="phone"
-                                />
-                            </Grid>
-                            <Grid item xs={12}>
-                                <TextField
-                                    required
-                                    fullWidth
-                                    id="email"
-                                    label="Email Address"
-                                    name="email"
-                                    autoComplete="email"
-                                />
-                            </Grid>
-                            <Grid item xs={12}>
-                                <TextField
-                                    required
-                                    fullWidth
-                                    name="password"
-                                    label="Password"
-                                    type="password"
-                                    id="password"
-                                    autoComplete="new-password"
-                                />
-                            </Grid>
-                            <Grid item xs={12}>
-                                <TextField
-                                    required
-                                    fullWidth
-                                    name="confirm password"
-                                    label="Confirm Password"
-                                    type="password"
-                                    id="confirmPassword"
-                                    autoComplete="confirm-password"
-                                />
-                            </Grid>
-                            <Grid item xs={12}>
-                                <FormControlLabel
-                                    control={<Checkbox value="allowExtraEmails" color="primary" />}
-                                    label="Accept privacy policy"
-                                />
-                            </Grid>
-                        </Grid>
-                        <Button
-                            type="submit"
-                            fullWidth
-                            variant="contained"
-                            sx={{ mt: 3, mb: 2 }}
-                        >
-                            Sign Up
-                        </Button>
-                        <Grid container justifyContent="flex-end">
-              <Grid item>
-             
-            <Link to="/login">Login</Link>
-          
-              </Grid>
-            </Grid>
-                    </Box>
-                </Box>
+                            <Button
+                                type="submit"
+                                fullWidth
+                                variant="contained"
+                                sx={{ mt: 3, mb: 2 }}
+                            >
+                                Sign Up
+                            </Button>
+                            <Grid container justifyContent="flex-end">
+                                <Grid item>
 
+                                    <Link to="/login">Login</Link>
+
+                                </Grid>
+                            </Grid>
+                        </Box>
+                    </Box>
+                </Grow>
             </Container>
         </ThemeProvider>
     );
